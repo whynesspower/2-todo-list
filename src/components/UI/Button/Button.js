c5fb2c93f6969a1f58ee2f761729a9aac33cfd00
Yashraj Shukla <yashrajshukla48@gmail.com>
2023-03-26 12:18:59 +0530

    working with CSS Modules.

--- src/components/UI/Button/Button.js
+++ src/components/UI/Button/Button.js
@@ -1,4 +1,8 @@
-import styled from "styled-components";
+import React from "react";
+
+// using CSS Modules now
+import styles from "./Button.module.css";
+// import styled from "styled-components";
 // npm install --save styled-components
 
 // by default width will be set to "auto"
@@ -6,41 +10,41 @@ import styled from "styled-components";
 
 // when media query fails, the size will be set to 100% of the screen.
 
-const Button = styled.button`
-  width: 100%;
+// const Button = styled.button`
+//   width: 100%;
 
-  font: inherit;
-  padding: 0.5rem 1.5rem;
-  border: 1px solid #8b005d;
-  color: white;
-  background: #8b005d;
-  box-shadow: 0 0 4px rgba(0, 0, 0, 0.26);
-  cursor: pointer;
+//   font: inherit;
+//   padding: 0.5rem 1.5rem;
+//   border: 1px solid #8b005d;
+//   color: white;
+//   background: #8b005d;
+//   box-shadow: 0 0 4px rgba(0, 0, 0, 0.26);
+//   cursor: pointer;
 
-  @media (min-width: 768px) {
-    width: auto;
-  }
-  &:focus {
-    outline: none;
-  }
+//   @media (min-width: 768px) {
+//     width: auto;
+//   }
+//   &:focus {
+//     outline: none;
+//   }
 
-  &:hover,
-  &:active {
-    background: #ac0e77;
-    border-color: #ac0e77;
-    box-shadow: 0 0 8px rgba(0, 0, 0, 0.26);
-  }
-`;
+//   &:hover,
+//   &:active {
+//     background: #ac0e77;
+//     border-color: #ac0e77;
+//     box-shadow: 0 0 8px rgba(0, 0, 0, 0.26);
+//   }
+// `;
 
 // you don't need to add the the below code which was just the default
 // behaviour of a button. the style.button will add the button behaviour for us
 
-// const Button = props => {
-//   return (
-//     <button type={props.type} className="button" onClick={props.onClick}>
-//       {props.children}
-//     </button>
-//   );
-// };
+const Button = (props) => {
+  return (
+    <button type={props.type} className={styles.button} onClick={props.onClick}>
+      {props.children}
+    </button>
+  );
+};
 
 export default Button;
